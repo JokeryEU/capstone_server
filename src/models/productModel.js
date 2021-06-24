2caaaf24568899ab154f77745d579fcb890895e9
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-25 02:33:08 +0300

    added product model

---
+++ src/models/productModel.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const productSchema = new Schema(
+  {
+    user: { 
+        type: Schema.Types.ObjectId, ref: "User"
+    }, 
+    name: { 
+        type: String,
+        trim: true,
+        required: true
+    },
+    image: {
+      type: String,
+      trim: true,
+      required: true,
+      
+    },
+    brand: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    reviews: [],
+    rating: {
+      type: Number,
+      trim: true,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      trim: true,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      trim: true,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+      default: 0,
+    },
+    
+    
+  },
+  { timestamps: true }
+);
+
+
+export default model("Product", productSchema);

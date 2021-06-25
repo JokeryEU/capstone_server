48fd7003e46d6bdb633c4ec768652f455b6eb625
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-25 15:02:29 +0300

    added bcrypt to user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,18 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
-const { Schema, model } = mongoose;
+const { Schema, model } = mongoose
 
 const userSchema = new Schema(
   {
-    firstName: { 
-        type: String,
-        trim: true,
-        required: true
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    lastName: { 
-        type: String,
-        trim: true,
-        required: true
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
     },
     email: {
       type: String,
@@ -23,7 +24,7 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: true,
-      minlength: [8, "Minimum length must be 8 chars"],
+      minlength: [8, 'Minimum length must be 8 chars'],
       trim: true,
     },
     profilePic: {
@@ -31,17 +32,46 @@ const userSchema = new Schema(
       trim: true,
     },
     role: {
-        type: String,
-        required: true,
-        enum: ["Admin", "User"],
-        default: "User",
-        immutable: true,
-      },
+      type: String,
+      required: true,
+      enum: ['Admin', 'User'],
+      default: 'User',
+      immutable: true,
+    },
     refreshToken: { type: String },
     googleId: { type: String },
   },
   { timestamps: true }
-);
+)
 
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(
+      this.password,
+      parseInt(process.env.SALT_ROUNDS)
+    )
+  }
+  next()
+})
 
-export default model("User", userSchema);
+userSchema.statics.checkCredentials = async function (email, password) {
+  const user = await this.findOne({ email })
+  if (user) {
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (isMatch) return user
+    else return null
+  } else return null
+}
+
+userSchema.methods.toJSON = function () {
+  const user = this
+
+  const userObject = user.toObject()
+
+  delete userObject.password
+  delete userObject.__v
+  delete userObject.refreshToken
+  return userObject
+}
+
+export default model('User', userSchema)

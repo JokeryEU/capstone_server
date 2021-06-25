3f78d5a4540f64c14758112667c0d24068248d8e
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-25 04:27:45 +0300

    added order model

---
+++ src/models/orderModel.js
@@ -0,0 +1,97 @@
+import mongoose from 'mongoose'
+
+const { Schema, model } = mongoose
+
+const orderSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          trim: true,
+          required: true,
+        },
+        image: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        price: {
+          type: Number,
+          trim: true,
+          required: true,
+        },
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, trim: true, required: true },
+      city: { type: String, trim: true, required: true },
+      country: { type: String, trim: true, required: true },
+      postalCode: { type: String, trim: true, required: true },
+      phoneNumber: { type: Number, trim: true, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    paymentResult: {
+      id: { type: String, trim: true },
+      status: { type: String, trim: true },
+      update_time: { type: String, trim: true },
+      email_adress: { type: String, trim: true },
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      trim: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      trim: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      trim: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+)
+
+export default model('Order', orderSchema)

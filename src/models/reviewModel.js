a1aca51150bea60ce453579492cd95b3ebd5f0c0
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-29 00:41:52 +0300

    updated reviewModel

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -1,26 +1,31 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const { Schema } = mongoose;
+const { Schema } = mongoose
 
 const reviewSchema = new Schema(
-    {
-        name: {
-            type: String,
-            trim: true,
-            required: true,
-        },
-        rating: {
-            type: Number,
-            trim: true,
-            required: true,
-        },
-        comment: {
-        type: String,
-        trim: true,
-        required: true,
-        },      
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    { timestamps: true }
-  );
-  
-  export default reviewSchema;
+    rating: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    comment: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+export default reviewSchema

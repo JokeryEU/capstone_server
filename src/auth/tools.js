d896da7f2536899707f0a7d193007010534fd788
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-28 20:55:08 +0300

    added jwt token  gen and verification tools

---
+++ src/auth/tools.js
@@ -0,0 +1,75 @@
+import jwt from 'jsonwebtoken'
+import UserModel from '../models/userModel.js'
+import ErrorResponse from '../middlewares/errorResponse.js'
+
+const generateJWT = (user) =>
+  new Promise((res, rej) =>
+    jwt.sign(
+      user,
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '15m' },
+      (error, token) => {
+        if (error) rej(error)
+        res(token)
+      }
+    )
+  )
+
+export const verifyJWT = (token) =>
+  new Promise((res, rej) =>
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+      if (error) rej(error)
+      res(decoded)
+    })
+  )
+
+const generateRefreshJWT = (user) =>
+  new Promise((res, rej) =>
+    jwt.sign(
+      user,
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '7d' },
+      (error, token) => {
+        if (error) rej(error)
+        res(token)
+      }
+    )
+  )
+
+const verifyRefreshToken = (token) =>
+  new Promise((res, rej) =>
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (error, decoded) => {
+      if (error) rej(error)
+      res(decoded)
+    })
+  )
+
+export const auth = async (user) => {
+  const newAccessToken = await generateJWT({ _id: user._id })
+  const newRefreshToken = await generateRefreshJWT({ _id: user._id })
+  user.refreshToken = newRefreshToken
+  await user.save()
+  return { accessToken: newAccessToken, refreshToken: newRefreshToken }
+}
+
+export const refreshJWT = async (oldRefreshToken) => {
+  const decoded = await verifyRefreshToken(oldRefreshToken)
+
+  const user = await UserModel.findOne({ _id: decoded._id })
+
+  if (!user) {
+    throw new ErrorResponse('Login again', 400)
+  }
+  const refreshToken = user.refreshToken
+
+  if (refreshToken !== oldRefreshToken) {
+    throw new ErrorResponse('Login again', 400)
+  }
+
+  const newAccessToken = await generateJWT({ _id: user._id })
+  const newRefreshToken = await generateRefreshJWT({ _id: user._id })
+
+  user.refreshToken = newRefreshToken
+  await user.save()
+  return { accessToken: newAccessToken, refreshToken: newRefreshToken }
+}

631d9f66ed9d82d0a5a8ee1c158fc889f7cb385c
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-29 01:58:10 +0300

    updated user schema to check if its a valid email

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -19,6 +19,10 @@ const userSchema = new Schema(
       type: String,
       trim: true,
       required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please add a valid email',
+      ],
       unique: true,
     },
     password: {

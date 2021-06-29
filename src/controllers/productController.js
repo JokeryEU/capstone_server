113edcf1d0c9ad23a0312997cb2f9cd1c74e62af
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-29 18:41:21 +0300

    updated edit product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -80,13 +80,13 @@ export const updateProduct = async (req, res, next) => {
     const product = await ProductModel.findById(req.params.id)
 
     if (product) {
-      product.name = name
-      product.price = price
-      product.description = description
-      product.image = req.file.path
-      product.brand = brand
-      product.category = category
-      product.countInStock = countInStock
+      product.name = name || product.name
+      product.price = price || product.price
+      product.description = description || product.description
+      product.image = req.file ? req.file.path : product.image
+      product.brand = brand || product.brand
+      product.category = category || product.category
+      product.countInStock = countInStock || product.countInStock
 
       const updatedProduct = await product.save()
       res.send(updatedProduct)
